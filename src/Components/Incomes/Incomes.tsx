34bd4adf09128789825ff3316d9ca2734d6bf14a
romulosemiao <r.semiao.a@gmail.com>
2022-09-01 12:45:56 -0300

    Adicionando o form das rotas de receitas e gastos.

--- src/Components/Incomes/Incomes.tsx
+++ src/Components/Incomes/Incomes.tsx
@@ -1,10 +1,56 @@
+import { useState } from "react";
+
 import { Header } from "../Header/Header";
 
+import "./Incomes.scss";
+
 const Incomes = () => {
+    const [value, setValue] = useState("");
+    const [date, setDate] = useState("");
+
+    const handleSubmitData = (e: React.SyntheticEvent<EventTarget>) => {
+        e.preventDefault();
+
+        console.log(value);
+        console.log(date);
+
+        setValue("");
+        setDate("");
+    };
+
     return (
         <>
             <Header />
-            <div>Receitas</div>
+            <div className="incomes">
+                <h2 className="incomes__title">Receitas</h2>
+
+                <form className="incomes__form" onSubmit={handleSubmitData}>
+                    <input
+                        id="value"
+                        type="number"
+                        placeholder="Valor"
+                        value={value}
+                        onChange={(e) => {
+                            setValue(e.target.value);
+                        }}
+                        required
+                    />
+
+                    <input
+                        id="date"
+                        type="date"
+                        value={date}
+                        onChange={(e) => {
+                            setDate(e.target.value);
+                        }}
+                        required
+                    />
+
+                    <button type="submit" onClick={() => {}}>
+                        enviar
+                    </button>
+                </form>
+            </div>
         </>
     );
 };
--- src/Components/Spends/Spends.tsx
+++ src/Components/Spends/Spends.tsx
@@ -1,11 +1,57 @@
+import { useState } from "react";
+
 import { Header } from "../Header/Header";
 
+import "./Spends.scss";
+
 const Spends = () => {
+    const [value, setValue] = useState("");
+    const [date, setDate] = useState("");
+
+    const handleSubmitData = (e: React.SyntheticEvent<EventTarget>) => {
+        e.preventDefault();
+
+        console.log(value);
+        console.log(date);
+
+        setValue("");
+        setDate("");
+    };
+
     return (
         <>
             <Header />
 
-            <div>Gastos</div>
+            <div className="spends">
+                <h2 className="spends__title">Gastos</h2>
+
+                <form className="spends__form" onSubmit={handleSubmitData}>
+                    <input
+                        id="value"
+                        type="number"
+                        placeholder="Valor"
+                        value={value}
+                        onChange={(e) => {
+                            setValue(e.target.value);
+                        }}
+                        required
+                    />
+
+                    <input
+                        id="date"
+                        type="date"
+                        value={date}
+                        onChange={(e) => {
+                            setDate(e.target.value);
+                        }}
+                        required
+                    />
+
+                    <button type="submit" onClick={() => {}}>
+                        enviar
+                    </button>
+                </form>
+            </div>
         </>
     );
 };
