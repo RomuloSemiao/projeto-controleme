c1c18b2c7737b9176b85ed2fe8f4b5a7611f05cb
romulosemiao <r.semiao.a@gmail.com>
2022-09-05 12:02:20 -0300

    Adicionando alterações gerais.

--- src/Components/Home/Home.tsx
+++ src/Components/Home/Home.tsx
@@ -7,15 +7,15 @@ const Home = () => {
             <Header />
             <div className="home">
                 <h2 className="home__title">
-                    Seja bem vindo ao <span>Controle</span> !
+                    Você está logado no <span>Controle</span> !
                 </h2>
 
                 <ul className="home__description">
                     <li>
-                        Acesse o menu Receitas para consultar as suas Receitas,
+                        Acesse o item Receitas para consultar as suas Receitas,
                     </li>
-                    <li>o menu Gastos para acessar os Gastos,</li>
-                    <li>o menu Lucros para consultar os seus Lucros,</li>
+                    <li>o item Gastos para acessar os Gastos,</li>
+                    <li>o item Lucros para consultar os seus Lucros,</li>
                     <li>e o item Sair para sair da sua conta!</li>
                 </ul>
             </div>
